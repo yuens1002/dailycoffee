894f3e813aea549d9892a7428b474dd599a3fb53
sunny yuen <yuens1002@gmail.com>
2016-02-06 14:13:51 -0800

    once upon a time (daily coffee & tea co)

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'X6uEyafjxopprFr6VQiCbLGXg');
+    define('CONSUMER_SECRET', 'hF5zHLr48zqyHxNUBqIa8mTch0CbWe2NrQKjcuXOTshgaYf6t2');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '430467123-qAbiYNKKt9qP13BVaQIe1KMy9NENzpShCXbIEmWv');
+    define('ACCESS_SECRET', 'u0jFivqQUPiYGLnb5F5abMUF9SwadyInbOoqO150r3pjX');
